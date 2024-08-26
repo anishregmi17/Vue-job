88aabaa1a2d601ee2e8c712f18822c176f620e03
anishregmi17 <anishregminaglibang@gmail.com>
2024-08-26 20:52:26 +0545

    tailwind setup

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,3 @@
-<script setup>
-import { ref } from "vue";
-
-const name = ref("Register Form");
-const formname = ref("");
-</script>
-
 <template>
-  <h1>{{ name }}</h1>
-  <form @submit.prevent="addTask">
-    <label for="formname">Full Name : </label>
-    <input type="text" id="formname" name="formname" v-model="formname" />
-    <br />
-    <label for="formname">Full Name : </label>
-    <input type="text" id="formname" name="formname" v-model="formname" />
-  </form>
+  <h1 class="text-2xl">Vue Jobs</h1>
 </template>
-
-<style scoped>
-h1 {
-  color: red;
-}
-</style>
