13ce07d85f72284d7866592bc71144897b754e6c
anishregmi17 <anishregminaglibang@gmail.com>
2024-08-26 15:40:22 +0545

    IMP:form added

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
-<script>
-export default {
-  setup() {
-    const name = "Anish";
-    return {
-      name,
-    };
-  },
-};
+<script setup>
+import { ref } from "vue";
+
+const name = ref("Register Form");
+const formname = ref("");
 </script>
 
 <template>
   <h1>{{ name }}</h1>
+  <form @submit.prevent="addTask">
+    <label for="formname">Full Name : </label>
+    <input type="text" id="formname" name="formname" v-model="formname" />
+    <br />
+    <label for="formname">Full Name : </label>
+    <input type="text" id="formname" name="formname" v-model="formname" />
+  </form>
 </template>
 
 <style scoped>
