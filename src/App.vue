284abb04209b3ef8ac964fefb03cfc6452998546
anishregmi17 <anishregminaglibang@gmail.com>
2024-08-26 15:07:04 +0545

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+  setup() {
+    const name = "Anish";
+    return {
+      name,
+    };
+  },
+};
+</script>
+
+<template>
+  <h1>{{ name }}</h1>
+</template>
+
+<style scoped>
+h1 {
+  color: red;
+}
+</style>
---
+++ src/Optionapi.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+  data() {
+    return {
+      name: "Anish Regmi",
+    };
+  },
+};
+</script>
+
+<template>
+  <h1>{{ name }}</h1>
+</template>
+
+<style scoped>
+h1 {
+  color: red;
+}
+</style>
